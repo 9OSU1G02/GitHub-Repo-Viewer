3eb012643037fcf2e6521c41648cc3970cbedf3b
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-03-14 23:17:49 +0700

    implement auth application

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:git_hub_repo_viewer/auth/domain/auth_failure.dart';
 import 'package:git_hub_repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:git_hub_repo_viewer/auth/infrastructure/infrastructure/dio_extensions.dart';
+import 'package:git_hub_repo_viewer/core/shared/encoder.dart';
 import 'package:oauth2/oauth2.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,6 +19,7 @@ class GithubOAuthHttpClient extends http.BaseClient {
 
 class GithubAuthenticator {
   final CredentialsStorage _credentialsStorage;
+  final Dio _dio;
   static const clientId = "f7fb4a16b00f2bbff87a";
   static const clientSecret = "52a0fb60e95e30fc52c2e985b4d1787d0a4374e2";
   static const scopes = ["repo", "user"];
@@ -24,13 +28,19 @@ class GithubAuthenticator {
   static final tokenEndpoint =
       Uri.parse("https://github.com/login/oauth/access_token");
   static final redirectUrl = Uri.parse("http://localhost:3000/callback");
-  GithubAuthenticator(this._credentialsStorage);
+  static final revocationEndpoint =
+      Uri.parse("https://api.github.com/applications/$clientId/tokens");
+  GithubAuthenticator(this._credentialsStorage, this._dio);
   Future<Credentials?> getSignedInCredentials() async {
     try {
       final storedCredentials = await _credentialsStorage.read();
       if (storedCredentials != null) {
         if (storedCredentials.canRefresh && storedCredentials.isExpired) {
-          // TODO: Refresh token
+          final failureOrCredentials = await refresh(storedCredentials);
+          return failureOrCredentials.fold(
+            (f) => null,
+            (credentials) => credentials,
+          );
         }
       }
       return storedCredentials;
@@ -70,6 +80,54 @@ class GithubAuthenticator {
       return left(const AuthFailure.server());
     } on AuthorizationException catch (e) {
       return left(AuthFailure.server("${e.error}: ${e.description}"));
+    }
+  }
+
+  Future<Either<AuthFailure, Unit>> signOut() async {
+    final accessToken = await _credentialsStorage
+        .read()
+        .then((credential) => credential?.accessToken);
+    final usernameAndPassword =
+        stringToBase64.encode("$clientId:$clientSecret");
+    try {
+      try {
+        _dio.deleteUri(revocationEndpoint,
+            data: {
+              "access_token": accessToken,
+            },
+            options: Options(
+              headers: {"Authorization": "basic $usernameAndPassword"},
+            ));
+      } on DioError catch (e) {
+        if (e.isConnectionError) {
+          // Ignore connection error
+        } else {
+          rethrow;
+        }
+      }
+      await _credentialsStorage.clear();
+      return right(unit);
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Credentials>> refresh(
+      Credentials credentials) async {
+    try {
+      final refreshCredentials = await credentials.refresh(
+        identifier: clientId,
+        secret: clientSecret,
+        httpClient: GithubOAuthHttpClient(),
+      );
+      await _credentialsStorage.save(refreshCredentials);
+      return right(refreshCredentials);
+    } on FormatException {
+      return left(const AuthFailure.server());
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server("${e.error}: ${e.description}"));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
     }
   }
 }
